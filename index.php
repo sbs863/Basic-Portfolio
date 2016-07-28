a872485d4782af28fd03726cdf8e121da8dc2cbe
Sam Smith <samtrey@gmail.com>
2016-07-28 12:51:36 -0500

    update index.html & style.css

---
+++ index.php
@@ -0,0 +1,5 @@
+<ul>
+	<li><a href="https://www.linkedin.com/in/sam-smith-844755125"</a>
+		<li><a href="https://github.com/sbs863"</a>
+	<li><a href="https://stackoverflow.com/users/6605956/sam-smith"></a>
+</ul>
