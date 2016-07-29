aecd7305062e3bc48eb5ad0f814f6eb8d951e45e
Sam Smith <samtrey@gmail.com>
2016-07-29 12:12:21 -0500

    add php and json files

--- index.php
+++ index.php
@@ -1,5 +1,1 @@
-<ul>
-	<li><a href="https://www.linkedin.com/in/sam-smith-844755125"</a>
-		<li><a href="https://github.com/sbs863"</a>
-	<li><a href="https://stackoverflow.com/users/6605956/sam-smith"></a>
-</ul>
+<?php include_once("index.html"); ?>
